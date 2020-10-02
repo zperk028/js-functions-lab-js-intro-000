9d8e52172b44a54c2dbb74a3de1e58e4251d52cf
Zachary Perkins <ezmac028@gmail.com>
2020-10-02 02:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
+
+function happyCustomHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}`)
+}
